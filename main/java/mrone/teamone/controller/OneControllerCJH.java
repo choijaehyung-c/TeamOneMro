6f15b9db705a48c3d3cb7d31c32fc72b4d73182d
choijaehyung-c <chlwogudc@naver.com>
2021-09-01 09:20:17 +0900

    clientOrderApiTest.jsp ClientCJH.xml ClientCJH.java ClientDaoCJH.java OneControllerCJH.java

---
+++ main/java/mrone/teamone/controller/OneControllerCJH.java
@@ -0,0 +1,48 @@
+package mrone.teamone.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import mrone.teamone.auth.Authentication;
+import mrone.teamone.beans.MroAccessBean;
+import mrone.teamone.utill.Encryption;
+import mrone.teamone.utill.ProjectUtils;
+
+@Controller
+@RequestMapping("/cjh")
+public class OneControllerCJH {
+	ModelAndView mav;
+	@Autowired
+	Authentication auth;
+	@Autowired
+	private ProjectUtils pu;
+	@Autowired
+	Encryption enc;
+	
+	
+	@PostMapping("/clientOrderApiTest")
+	public ModelAndView accessOutMro(@ModelAttribute MroAccessBean ma) {
+		mav = auth.accessOutMroCtl(ma);
+		return mav;
+	}
+	
+	
+}
