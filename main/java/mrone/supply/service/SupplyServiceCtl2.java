9f3399952dc7588b3065200aee2b416850f4ad03
DESKTOP-JMK42DD\jos77 <jos771@nate.com>
2021-09-01 15:27:44 +0900

    공급사

--- main/java/mrone/supply/service/SupplyServiceCtl2.java
+++ main/java/mrone/supply/service/SupplyServiceCtl2.java
@@ -1,5 +1,28 @@
 package mrone.supply.service;
 
-public class SupplyServiceCtl2 {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
+
+import mrone.teamone.beans.ProductBean;
+
+
+@Service
+public class SupplyServiceCtl2 {
+	ModelAndView mav;
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	
+	
+	public List<ProductBean> supplyProductListCtl(ProductBean pb) {
+		List<ProductBean> productBean;
+		productBean = sqlSession.selectList("supplyProductListCtl", pb);
+		
+		return productBean;
+	}
+	
+	
 }
--- main/java/mrone/teamone/controller/RestApiController5.java
+++ main/java/mrone/teamone/controller/RestApiController5.java
@@ -1,10 +1,34 @@
 package mrone.teamone.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import mrone.supply.service.SupplyServiceCtl2;
+import mrone.teamone.beans.ProductBean;
 
 @RestController
 @RequestMapping("/vue5")
 public class RestApiController5 {
+	ModelAndView mav;
+	@Autowired
+	SupplyServiceCtl2 ssc2;
+	
+
+	@PostMapping("/SupplyProductListForm")
+	public ModelAndView supplyProductListForm() {
+		return mav;
+	}
+	
+	@PostMapping("/productBean")
+	public List<ProductBean> supplyProductListCtl(@RequestBody List<ProductBean> SupplyPrList){
+		return ssc2.supplyProductListCtl(SupplyPrList.get(0));
+		
+	}
 
 }
