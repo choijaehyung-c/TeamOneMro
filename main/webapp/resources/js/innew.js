d04acae12952ad7efbafc99d6df4410567bbba9a
youjeong-in <i_innew0731@naver.com>
2021-09-02 09:57:01 +0900

    innew/js 수정

--- main/webapp/resources/js/innew.js
+++ main/webapp/resources/js/innew.js
@@ -21,6 +21,40 @@ function supplyList(){
 	alert("공급사목록을 불러옵니다.");
 }
 
+//mro에서 주문대기리스트 불러오기
+function mroOrderList(){
+	
+	postAjaxJson('vue2/mroOrderListForm','getWaitOrderListM',clientData="")
+	alert("주문목록을 불러옵니다.");
+}
+
+//주문목록 찍어주는 펑션
+function getWaitOrderListM(data){
+	
+	let space = document.getElementById("mOrderList");
+	let HTML = "<div>주문목록</div>";
+	
+	for(i=0; i<data.length; i++){
+		HTML += "<div id='orderList' name='orderList'>"+"[주문날짜 : " + data[i].os_DATE +"] [주문번호 : " + data[i].os_CODE+ "] [고객사 : " + data[i].cl_NAME + "] [상태 : " + data[i].os_STATE +"]</div>";
+		
+	
+		
+		space.innerHTML = HTML;
+	}
+}
+
+
+//주문번호의 상세정보 불러오기
+function orderDetail(){
+	
+	const osCode = document.getElementsByName("OD_OSCODE")[0];
+	
+	alert(osCode);
+	let f = makeForm("mroGetOrderDetail","post");
+	f.appendChild(osCode);
+	document.body.appendChild(f);
+	f.submit();
+}
 
 /*const orderList = new Vue({
 	el: "#mOrderList",
@@ -50,38 +84,5 @@ function supplyList(){
 }*/
 
 
-//mro에서 주문대기리스트 불러오기
-function mroOrderList(){
-	
-	postAjaxJson('vue2/mroOrderListForm','getWaitOrderListM',clientData="")
-	/*let f = makeForm("/iyj/mroOrderListForm","post");
-	document.body.append(f);
-	f.submit();*/
-	alert("주문목록을 불러옵니다.");
-}
-
-function getWaitOrderListM(data){
-	alert(data);
-	let space = document.getElementById("mOrderList");
-	
-	const HTML = "<div>주문목록</div>";
-	
-	for(i=0; i<data.length; i++){
-		HTML += "<div id='orderList' name='orderList'>"+ data[i]+"</div>"
-		
-		"<input type='checkbox' id='checkbox' name='invit' value='"+ jsonData[i].userId + "'/>" + "<div class='contentsSpaceId'>"+ jsonData[i].userId + "</div><div class='contentsSpaceName'>"+ jsonData[i].userName + "</div><div class='contentsSpaceMail'>"+  jsonData[i].userMail + "</div><br>";
-	}
-}
 
 
-//주문번호의 상세정보 불러오기
-function orderDetail(){
-	
-	const osCode = document.getElementsByName("OD_OSCODE")[0];
-	
-	alert(osCode);
-	let f = makeForm("mroGetOrderDetail","post");
-	f.appendChild(osCode);
-	document.body.appendChild(f);
-	f.submit();
-}
