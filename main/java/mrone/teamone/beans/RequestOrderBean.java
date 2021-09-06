bee1fd6c157db034dc9afe2e7de353efd579c418
youjeong-in <i_innew0731@naver.com>
2021-09-06 18:11:29 +0900

    requestBean 수정

--- main/java/mrone/teamone/beans/RequestOrderBean.java
+++ main/java/mrone/teamone/beans/RequestOrderBean.java
@@ -10,6 +10,7 @@ public class RequestOrderBean {
 	private String re_code;
 	private String re_oscode;
 	private String re_clcode;
+	private String cl_name;
 	private String re_spcode;
 	private String re_date;
 	private String re_state;
--- main/java/mrone/teamone/beans/RequestOrderDetailBean.java
+++ main/java/mrone/teamone/beans/RequestOrderDetailBean.java
@@ -8,6 +8,7 @@ import lombok.Data;
 @Data
 public class RequestOrderDetailBean {
 		private String rd_prspcode;
+		private String sp_name;
 		private String rd_recode;
 		private String pr_code;
 		private String pr_name;
